9e26a5a8685ad91d173aa7527a18cff7330a5f12
Przemyslaw Pacek <ppacek@op.pl>
2026-02-24 21:10:24 +0100

    Added to localStorage job application

---
+++ src/lib/stores/applications.ts
@@ -0,0 +1,21 @@
+import { writable } from "svelte/store";
+
+export type Application = {
+  jobId: number;
+  appliedAt: number;
+};
+
+const stored =
+  typeof localStorage !== "undefined"
+    ? localStorage.getItem("applications")
+    : null;
+
+const initial: Application[] = stored ? JSON.parse(stored) : [];
+
+export const applications = writable<Application[]>(initial);
+
+applications.subscribe((value) => {
+  if (typeof localStorage !== "undefined") {
+    localStorage.setItem("applications", JSON.stringify(value));
+  }
+});
